7609e59bcd2f01d74a88c22d562d502e3ea8f892
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-01-20 20:50:18 -0500

    second commit with testing

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
 import {Link} from "@reach/router";
 
+console.log("TESTIN TESTING TESTING ")
+
 const Main = () => {
     //if i want it to run some function upon rendering of this component, i want to use useEffect
     const [allplayers, setallplayers] = useState([])
